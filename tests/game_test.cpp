e0e28618e90fc593c51beb530884b5347563f6a4
Alexander Joham <alex.joham@tum.de>
2026-09-12 20:28:58 +0200

    Add new helper method layoutWith in game_test for easier testing

--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -57,26 +57,30 @@ namespace {
         check(match, acc_case.what);
     }
 
+    Game::MineLayout layoutWith(std::initializer_list<BoardCoord> mines) {
+        Game::MineLayout test_layout{};
+        for (BoardCoord mine : mines) {
+            test_layout[mine.row][mine.column] = true;
+        }
+        return test_layout;
+    }
+
     void runCases() {
         // Test 1: Mine in { 0, 0 } is handled correctly
-        Game::MineLayout test_layout_1{};
-        test_layout_1[0][0] = true;
         Game::Board exp_field_1 = getEmptyRevealedBoard();
         exp_field_1[0][0] = { false, false, FieldType::MINE };
         exp_field_1[1][0] = { false, false, FieldType::ONE };
         exp_field_1[1][1] = { false, false, FieldType::ONE };
         exp_field_1[0][1] = { false, false, FieldType::ONE };
-        AcceptCase acc_case_1 = { test_layout_1, exp_field_1, "Mine in { 0, 0 } is handled correctly"};
+        AcceptCase acc_case_1 = { layoutWith({ { 0, 0 } }), exp_field_1, "Mine in { 0, 0 } is handled correctly"};
 
         // Test 2: Eight mines around field { 4, 4 } is handled correctly
-        Game::MineLayout test_layout_2{};
         Game::Board exp_field_2 = getEmptyRevealedBoard();
         for (size_t i = 3; i <= 5; i++) {
             for (size_t j = 3; j <= 5; j++) {
                 if (i == j && i == 4) {
                     continue;
                 }
-                test_layout_2[i][j] = true;
                 exp_field_2[i][j] = { false, false, FieldType::MINE };
             }
         }
@@ -99,7 +103,11 @@ namespace {
         exp_field_2[4][2] = { false, false, FieldType::THREE };
         exp_field_2[4][6] = { false, false, FieldType::THREE };
         exp_field_2[6][4] = { false, false, FieldType::THREE };
-        AcceptCase acc_case_2 = { test_layout_2, exp_field_2, "Eight mines around field { 4, 4 } is handled correctly"};
+        AcceptCase acc_case_2 = { layoutWith({
+            { 3, 3 }, { 3, 4 }, { 3, 5 },
+            { 4, 3 },            { 4, 5 },
+            { 5, 3 }, { 5, 4 }, { 5, 5 },
+        }), exp_field_2, "Eight mines around field { 4, 4 } is handled correctly"};
         
         checkAcceptCase(acc_case_1);
         checkAcceptCase(acc_case_2);
@@ -119,16 +127,12 @@ namespace {
         check(rem_mines == 0, "Random generated field has 10 mines");
 
         // Test 4: Hitting a number cell only reveals this cell
-        Game::MineLayout test_layout_3{};
-        test_layout_1[0][0] = true;
-        game = Game(test_layout_3);
+        game = Game(layoutWith({ { 0, 0 } }));
         std::vector<RevealedCell> revealed_cells = game.makeMove(0, 1);
         check(revealed_cells.size() == 1, "Hitting a number cell only reveals this cell");
 
         // Test 5: game.MakeMove does not reveal duplicate cells
-        Game::MineLayout test_layout_4{};
-        test_layout_4[0][0] = true;
-        game = Game(test_layout_4);
+        game = Game(layoutWith({ { 0, 0 } }));
         revealed_cells = game.makeMove(8, 8);
         std::set<std::pair<size_t, size_t>> revealed_set;
         int duplicates = 0;
@@ -143,9 +147,7 @@ namespace {
         check(duplicates == 0, "game.MakeMove does not reveal duplicate cells (found " + std::to_string(duplicates) + " duplicates) for a setup with one mine in (0,0)");
 
         // Test 6: flagged cells are not revealed
-        Game::MineLayout test_layout_5{};
-        test_layout_5[0][0] = true;
-        game = Game(test_layout_5);
+        game = Game(layoutWith({ { 0, 0 } }));
         for (size_t i = 0; i < game.kFieldSize; i++) {
             for (size_t j = 0; j < game.kFieldSize; j++) {
                 if ((i == 0 && j == 0) || (i == 8 && j == 8)) {
@@ -158,22 +160,17 @@ namespace {
         check(revealed_cells.size() == 1, "flagged cells are not revealed after clicking on neutral cell (revealed " + std::to_string(revealed_cells.size()) + " cells instead of 1) for a setup with one mine in (0,0) and all other cells flagged except (8,8). Revealed (8,8).");
 
         // Test 7: flagged cell cannot be revealed
-        Game::MineLayout test_layout_6{};
-        test_layout_6[4][4] = true;
-        game = Game(test_layout_6);
+        game = Game(layoutWith({ { 4, 4 } }));
         game.toggleFlag(4,4);
         revealed_cells = game.makeMove(4, 4);
         check(revealed_cells.size() == 0, "flagged cells are not revealed by clicking on it (revealed " + std::to_string(revealed_cells.size()) + " cells instead of 0) for a setup with one flagged cell in (4,4). Tried to revealed (4,4).");
 
         // Test 8: Hitting a number 6 cell only reveals this cell
-        Game::MineLayout test_layout_7{};
-        test_layout_7[3][3] = true;
-        test_layout_7[3][4] = true;
-        test_layout_7[3][5] = true;
-        test_layout_7[4][3] = true;
-        test_layout_7[4][5] = true;
-        test_layout_7[5][3] = true;
-        game = Game(test_layout_7);
+        game = Game(layoutWith({
+            { 3, 3 }, { 3, 4 }, { 3, 5 },
+            { 4, 3 },            { 4, 5 },
+            { 5, 3 },
+        }));
         revealed_cells = game.makeMove(4, 4);
         check(revealed_cells.size() == 1, "Hitting a number 6 cell only reveals this cell");
     }
